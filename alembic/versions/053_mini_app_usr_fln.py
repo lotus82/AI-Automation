7e61d1e8be527cd21970fae3c69eac15521acaac
lotus82 <lotus.it@mail.ru>
2026-04-27 02:10:41 +0400

    fix имя пользователей mini app

--- alembic/versions/053_mini_app_usr_fln.py
+++ alembic/versions/053_mini_app_usr_fln.py
@@ -5,7 +5,8 @@ from __future__ import annotations
 import sqlalchemy as sa
 from alembic import op
 
-revision = "053_mini_app_users_first_last_name"
+# revision id must fit alembic_version.version_num (VARCHAR(32) in this project)
+revision = "053_mini_app_usr_fln"
 down_revision = "052_mini_app_users_birth_date"
 branch_labels = None
 depends_on = None
